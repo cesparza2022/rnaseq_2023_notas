598e7dae49b877e2149dfee9a9d803540c48a21d
cesparza2022 <100377746+cesparza2022@users.noreply.github.com>
2023-02-01 04:34:00 -0600

    Create 03_disc_biocndtr.R

---
+++ R/03_disc_biocndtr.R
@@ -0,0 +1,18 @@
+### Discusion de nuevos paquetes en bioconductor
+## Cesar Esparza
+
+## SpliceWiz
+# Autores: Alex Chit Hei Wong, Ulf Schmitz, William Ritchie
+
+# Este paquete cuantifica los ASE, calculando el porcentaje de empalme (PSI) 
+# mediante lecturas de union y la retencion de intrones mediante la cuantificacion
+# basada en IRFinder.
+
+# rank: 2121/2183
+
+# Opinion 
+# Me llamo la atencion que explora el tema de splicing alternativo.
+# Resulta que este paquete se caracteriza por su velocidad para cuatificar eventos de splicing usando alineamientos de isoformas especificas. 
+# Una de las caracteristicas de la actualizacion fue la adicion de filtros apra remover datos de poca confianza. 
+
+
